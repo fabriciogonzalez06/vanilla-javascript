d8bdcc19d8b2f14cf07ce9c498b7bbc907cc0303
Fabricio gonzalez <angelfabriciogonzalez@gmail.com>
2020-06-14 19:16:04 -0600

    nuevos cambios

--- javascript/app.js
+++ javascript/app.js
@@ -5,6 +5,32 @@ const btnMostrar = document.getElementById("mostrar");
 //referencia de el div con la calse hworld
 const hworld = document.querySelector("#hworld");
 
+//objeto json de javascript
+const lenguajes = [
+    { nombre: "javascript", tipo: "web y backend con nodejs" },
+    { nombre: "java", tipo: "proposito general" },
+    { nombre: "python", tipo: "proposito general" }
+];
+
+
+function mostrarLenguajesEnConsola() {
+    for (let i = 0; i < lenguajes.length; i++) {
+        console.log(lenguajes[i]);
+    }
+}
+mostrarLenguajesEnConsola();
+
+
+
+
+
+
+//peticion http a una api para mostrar los datos en consola
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((res => res.json()))
+    .then((respuesta) => {
+        console.log(respuesta);
+    });
 
 
 //escuchar evento click del boton ocultar
